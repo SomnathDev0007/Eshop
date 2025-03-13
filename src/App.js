9e6321597fe33d3369a5322c19f88658d0cfcadc
Somnath Dev <167564966+SomnathDev0007@users.noreply.github.com>
2025-03-13 19:00:07 +0530

    Header component1 (#2)
    * initial commit
    
    * Added Header component
    
    ---------
    
    Co-authored-by: SomnathDev0007 <somnath007@gmail.com>

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
-import './App.css';
-
+import "./App.css";
+import Header from "./Header";
 function App() {
   return (
     <div className="App">
-   <div className="header_logo"></div>
-   <div className="temporary"></div>
+      <Header/>
     </div>
   );
 }
